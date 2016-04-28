fea6c55b328c1f35376e7ad506cee69d3b34d428
tohir <thohiru.omoloye@talentbase.ng>
2016-04-28 10:07:10 +0100

    Updated sending multiple sms

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ require_once "vendor/autoload.php";
 
 function getDB() {
     $dbhost = "localhost";
-    $dbuser = "smshitne_tundsm"; //"root";
-    $dbpass = "smsTunde?#123_olajire"; //"root";
+//    $dbuser = "smshitne_tundsm"; //"root";
+//    $dbpass = "smsTunde?#123_olajire"; //"root";
+    $dbuser = "root";
+    $dbpass = "root";
     $dbname = "smshitne_smshit";
 
     $mysql_conn_string = "mysql:host=$dbhost;dbname=$dbname";
@@ -53,17 +55,16 @@ $app->get('/', function() use($app) {
     getDB();
     echo "Welcome to SMSHit API v1.0<br><br> "
     . "<b>Simple Documentation</b> <br>"
-            . "Use <b><i>http://api.smshit.net/index.php/push_sms/:account_key/:message/:destination/:sender_id</i></b> to send SMS<br><br>"
-            . " <b><u>Parameters</u></b><br>"
-            . "<ul>"
-            . "<li>account_key: - you account key on smshit"
-            . "<li>message: - Message to be sent"
-            . "<li>destination: - Receiver's phone number"
-            . "<li>sender_id: - Sender ID"
-            . "</ul>"
-            . "<p style='color:red'><b>Note: All parameters must be url encoded</b></p>"
-            
-            . "Example: http://api.smshit.net/index.php/push_sms/a4f85b620e/Welcome%20to%20SMSHit/08037816587/Testing" ;
+    . "Use <b><i>http://api.smshit.net/index.php/push_sms/:account_key/:message/:destination/:sender_id</i></b> to send SMS<br><br>"
+    . " <b><u>Parameters</u></b><br>"
+    . "<ul>"
+    . "<li>account_key: - you account key on smshit"
+    . "<li>message: - Message to be sent"
+    . "<li>destination: - Receiver's phone number"
+    . "<li>sender_id: - Sender ID"
+    . "</ul>"
+    . "<p style='color:red'><b>Note: All parameters must be url encoded</b></p>"
+    . "Example: http://api.smshit.net/index.php/push_sms/a4f85b620e/Welcome%20to%20SMSHit/08037816587/Testing";
 });
 
 // add new Route 
@@ -84,17 +85,16 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
     $phoneNumber = '';
     $splitNumbers = explode(":", $recipient);
     $recipientCount = count($splitNumbers);
-    
-    if ((int) $recipientCount > 1){
-        foreach($splitNumbers as $phone){
+
+    if ((int) $recipientCount > 1) {
+        foreach ($splitNumbers as $phone) {
             $phoneNumber .= checkPhoneNumber($phone) . ":";
         }
         $phoneNumber = rtrim($phoneNumber, ":");
-    }else{
+    } else {
         $phoneNumber = checkPhoneNumber($recipient);
     }
-    
-    
+
 
     try {
         $db = getDB();
@@ -106,10 +106,32 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
         $sth->execute();
 
         $user = $sth->fetch(PDO::FETCH_OBJ);
-        
+
         // Compute SMS count
-        $sms_count = strlen($message)/160;
-        $sms_count = $sms_count > intval($sms_count) ? intval($sms_count + 1) * $recipientCount : $sms_count * $recipientCount;
+        $sms_count = 1;
+        $meslen = strlen($message);
+        if ($meslen >160 && $meslen < 306 )
+	  {
+		  $sms_count = 2;
+	  }
+	  else if ($meslen >305 && $meslen < 458)
+	  {
+		 $sms_count = 3;
+	  }
+	  elseif($meslen >457 && $meslen < 611)
+	  {
+		  $sms_count = 4;
+	  }
+	  elseif($meslen >610 && $meslen < 751)
+	  {
+		  $sms_count = 5;
+	  }
+	  elseif($meslen >750 && $meslen < 891)
+	  {
+		  $sms_count = 6;
+	  }
+          
+        $sms_count = $sms_count * $recipientCount;
 
         if ($user) {
             if ((int) $user->credit > $sms_count) {
@@ -118,6 +140,7 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
                 $time = date('h:i:s');
                 $ampm = date('A');
                 $today_date = date('Y-m-d');
+                $mid = rand(10, 20000);
 
                 // build api url
                 $url = $gateway . "u=" . urlencode($username)
@@ -144,15 +167,22 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
                     ));
                 }
 
-               
+
                 // log inside outbox
                 try {
-                     $db = getDB();
-                $stmt = $db->prepare("INSERT INTO smsoutbox (pnumber, sname, date, message, status, sid, smscount, country, time, ampm, mid) VALUES ($recipient, '$senderId', $today_date, '$message', '$status', $user->id, 1, 'NG', '$time', '$ampm', null)"); 
-                    $stmt->execute();
-                    
+                    $db = getDB();
+                    if ((int) $recipientCount > 1) {
+                        foreach ($splitNumbers as $phone) {
+                            $stmt = $db->prepare("INSERT INTO smsoutbox (pnumber, sname, date, message, status, sid, smscount, country, time, ampm, mid) VALUES ($phone, '$senderId', '$today_date', '$message', '$status', $user->id, 1, 'NG', '$time', '$ampm', '$mid')");
+                            $stmt->execute();
+                        }
+                    } else {
+                        $stmt = $db->prepare("INSERT INTO smsoutbox (pnumber, sname, date, message, status, sid, smscount, country, time, ampm, mid) VALUES ($recipient, '$senderId', '$today_date', '$message', '$status', $user->id, 1, 'NG', '$time', '$ampm', '$mid')");
+                        $stmt->execute();
+                    }
+
                     // deduct SMS
-                    $sql = $db->prepare("UPDATE users SET credit=credit-1 WHERE id=:user_id");
+                    $sql = $db->prepare("UPDATE users SET credit=credit-{$sms_count} WHERE id=:user_id");
                     $sql->bindParam(':user_id', $user->id, PDO::PARAM_INT);
                     $sql->execute();
                     
@@ -160,7 +190,6 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
                     echo $ex->getMessage();
                     exit;
                 }
-
 
 
                 echo $respnse_str;
@@ -172,9 +201,9 @@ $app->get("/push_sms/:acct_key/:message/:recipient/:sname", function($acct_key, 
             }
         } else {
             echo json_encode(array(
-                    "status" => false,
-                    "message" => "Invalid account key"
-                ));
+                "status" => false,
+                "message" => "Invalid account key"
+            ));
             //throw new PDOException('No records found.');
         }
     } catch (Exception $ex) {
